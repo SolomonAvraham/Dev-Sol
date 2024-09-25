e801ff8f98675901d92cf4cec66f5aec8fb61146
SolomonAvraham <Selmon_a@hotmail.com>
2024-09-25 23:29:07 +0300

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import NavBar from "src/components/features/navBar/NavBar";
-import Footer from "src/components/features/footer/footer";
-
+import NavBar from "@/components/features/navBar/NavBar";
+import Footer from "@/components/features/footer/footer";
+ 
 export const metadata: Metadata = {
   title: "Dev-Sol Web Design + Development",
   description:
